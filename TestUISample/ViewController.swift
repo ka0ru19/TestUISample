a42945d37c03004efc5703a0ae7db68ccdb68112
井上航 <k.prfm19@gmail.com>
2018-10-08 04:00:49 +0900

    Initial Commit

---
+++ TestUISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestUISample
+//
+//  Created by Wataru Inoue on 2018/10/08.
+//  Copyright © 2018 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
